d104fca8c9bc872bfeb462c55b6d20bd5d44d7bb
gamzakitty <96065039+gamzakitty@users.noreply.github.com>
2024-07-24 17:31:34 +0900

    Update SecurityConfig.java

--- src/main/java/ac/su/inclassspringsecurity/config/SecurityConfig.java
+++ src/main/java/ac/su/inclassspringsecurity/config/SecurityConfig.java
@@ -1,5 +1,25 @@
+package ac.su.inclassspringsecurity.config;
+
+import ac.su.inclassspringsecurity.config.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity  // URL 요청에 대한 Spring Security 동작 활성화
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final TokenProvider tokenProvider;
@@ -7,33 +27,50 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(
-                (authorizeHttpRequests) ->
-                    authorizeHttpRequests
-                        .requestMatchers("/users/login", "/csrf-token", "/products-temp/**").permitAll()
-                        .requestMatchers("/products-temp/**").hasAnyRole("SUPER_ADMIN", "ADMIN")
-                        .anyRequest().authenticated()
-            )
+    .authorizeHttpRequests(
+        (authorizeHttpRequests) ->
+            authorizeHttpRequests
+                .requestMatchers("/users/login", "/csrf-token", "/products-temp/**").permitAll()
+                .requestMatchers("/products-temp/**").hasAnyRole("SUPER_ADMIN", "ADMIN")
+                .anyRequest().authenticated()
+    )
+
+//            .csrf(csrf -> csrf
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//            )
             .csrf(
-                (csrf) -> csrf
-                    .ignoringRequestMatchers(
-                        new AntPathRequestMatcher("/api/**"),
-                        new AntPathRequestMatcher("/users/login")
-                    )
-            )
+    (csrf) -> csrf
+        .ignoringRequestMatchers(
+            new AntPathRequestMatcher("/api/**"),
+            new AntPathRequestMatcher("/users/login")
+        )
+)
+
+//                (csrf) ->
+//                    csrf.ignoringRequestMatchers(
+//                        // 필요 시 특정 페이지 CSRF 토큰 무시 설정
+//                        new AntPathRequestMatcher("/h2-console/**")
+//                        // , new AntPathRequestMatcher("/login")
+//                        // , new AntPathRequestMatcher("/logout")
+//                        // , new AntPathRequestMatcher("/signup")
+//                    )
+//            )
             .headers(
                 (headers) ->
                     headers.addHeaderWriter(
                         new XFrameOptionsHeaderWriter(
-                            XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                            // X-Frame-Options 는 웹 페이지 내에서 다른 웹 페이지 표시 허용 여부 제어
+                            XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN  // 동일 도메인 내에서 표시 허용
                         )
                     )
             )
             .formLogin(
                 (formLogin) ->
-                    formLogin
+                    formLogin  // Controller 에 PostMapping URL 바인딩이 없어도
+                               // POST 요청을 아래 라인에서 수신하고 인증 처리
                         .loginPage("/users/login")
                         .defaultSuccessUrl("/")
+//                AbstractHttpConfigurer::disable
             )
             .logout(
                 (logout) ->
@@ -43,17 +80,19 @@ public class SecurityConfig {
                         .invalidateHttpSession(true)
             )
             .sessionManagement(
-                (sessionConfig) ->
-                    sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                (sessionConfig) -> {
+                    sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                }
             )
             .addFilterBefore(
-                tokenAuthenticationFilter(),
+                tokenAuthenticationFilter(),  // 토큰을 username, password 검사보다 먼저 검사한다.
                 UsernamePasswordAuthenticationFilter.class
-            );
-
+            )
+        ;
         return http.build();
     }
 
+    // passwordEncoder 빈 등록
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
