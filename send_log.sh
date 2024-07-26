97283cc068e4e0ac001bbdec2f11140b2dbe5917
gamzakitty <96065039+gamzakitty@users.noreply.github.com>
2024-07-26 17:14:51 +0900

    Update send_log.sh

--- send_log.sh
+++ send_log.sh
@@ -4,7 +4,7 @@
 SUBJECT="Daily Resource Usage Log"
 TO="1015hae@naver.com"
 FROM="swuswu_pj2@example.com"
-LOG_FILE="/home/ubuntu/log_test/oldlog/resource_usage.log.1.gz"
+LOG_FILE="/home/ubuntu/log_test/oldlog/resource_usage.log.2.gz"
 BODY="Please find the attached log file for resource usage."
 
 # 압축된 로그 파일을 이메일로 전송
