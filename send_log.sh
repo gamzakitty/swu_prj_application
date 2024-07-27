ed19fd4fffa2a0d9cbb3a6753e581e0fe61d184f
gamzakitty <96065039+gamzakitty@users.noreply.github.com>
2024-07-27 18:00:13 +0900

    Update send_log.sh

--- send_log.sh
+++ send_log.sh
@@ -9,6 +9,16 @@ BODY="Please find the attached log file for resource usage."
 
 # 이메일 내용 및 첨부파일을 위한 임시 파일 생성
 TMPFILE=$(mktemp /tmp/email.XXXXXX)
+
+# 디버깅 로그 추가
+echo "TMPFILE: $TMPFILE"
+echo "LOG_FILE: $LOG_FILE"
+echo "Checking LOG_FILE permissions:"
+ls -l "$LOG_FILE"
+
+echo "Checking msmtp configuration file permissions:"
+ls -l /var/lib/jenkins/.msmtprc
+
 {
     echo "Subject: $SUBJECT"
     echo "To: $TO"
