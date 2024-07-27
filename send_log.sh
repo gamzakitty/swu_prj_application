e03314430fe0c48e6094881cb8079eb23bb25b2e
gamzakitty <96065039+gamzakitty@users.noreply.github.com>
2024-07-27 19:42:32 +0900

    Update send_log.sh

--- send_log.sh
+++ send_log.sh
@@ -17,7 +17,7 @@ echo "Checking LOG_FILE permissions:"
 ls -l "$LOG_FILE"
 
 echo "Checking msmtp configuration file permissions:"
-ls -l /var/lib/jenkins/msmtp/.msmtprc
+ls -l ./.msmtprc
 
 {
     echo "Subject: $SUBJECT"
@@ -40,7 +40,7 @@ ls -l /var/lib/jenkins/msmtp/.msmtprc
 } > "$TMPFILE"
 
 # 이메일 전송
-msmtp --file=/var/lib/jenkins/msmtp/.msmtprc --from="$FROM" -t < "$TMPFILE"
+msmtp --file=./.msmtprc --from="$FROM" -t < "$TMPFILE"
 
 # 임시 파일 삭제
 rm "$TMPFILE"
