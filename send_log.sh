c934fd620737c6fadb6713c52538ed659fe694ac
gamzakitty <96065039+gamzakitty@users.noreply.github.com>
2024-07-27 13:33:13 +0900

    Update send_log.sh

--- send_log.sh
+++ send_log.sh
@@ -7,5 +7,17 @@ FROM="929kong@gmail.com"
 LOG_FILE="/home/ubuntu/log_test/oldlog/resource_usage.log.2.gz"
 BODY="Please find the attached log file for resource usage."
 
-# 압축된 로그 파일을 이메일로 전송
-echo "$BODY" | mail -s "$SUBJECT" -r "$FROM" -A "$LOG_FILE" "$TO"
+# 이메일 내용 및 첨부파일을 위한 임시 파일 생성
+TMPFILE=$(mktemp /tmp/email.XXXXXX)
+echo "Subject: $SUBJECT" >> $TMPFILE
+echo "To: $TO" >> $TMPFILE
+echo "From: $FROM" >> $TMPFILE
+echo "" >> $TMPFILE
+echo "$BODY" >> $TMPFILE
+uuencode "$LOG_FILE" $(basename "$LOG_FILE") >> $TMPFILE
+
+# 이메일 전송
+cat $TMPFILE | msmtp --read-envelope-from --auto-from "$TO"
+
+# 임시 파일 삭제
+rm $TMPFILE
